b831e441c054fb56384c778592dd2e43dc91c5e2
Brian Tai <brian@reflexionhealth.com>
2016-04-11 13:43:20 -0700

    date: scan should take in a pointer to a date to scan the value

--- date/date.go
+++ date/date.go
@@ -169,13 +169,13 @@ func (d Date) String() string {
 }
 
 // Implements sql.Scanner interface
-func (d Date) Scan(src interface{}) error {
+func (d *Date) Scan(src interface{}) error {
 	t, ok := src.(time.Time)
 	if !ok {
 		return errors.New("unstable/date: scan value was not a Time")
 	}
 
-	d = From(t)
+	*d = From(t)
 	return nil
 }
 
